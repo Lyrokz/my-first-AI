f52a95803f23f5a541ddafa5a6b4ebe1a46882f9
W0lfsBlut <cneumeier@live.de>
2016-11-27 15:52:45 +0100

    Update index.js

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -68,12 +68,29 @@ exports.handle = function handle(client) {
       })
     },
   })
+const handleGreeting = client.createStep({
+  satisfied() {
+    return false
+  },
 
+  prompt() {
+    client.addResponse('greeting')
+    client.done()
+  }
+})
   client.runFlow({
-    classifications: {},
+    classifications: {
+      // Add a greeting handler with a reference to the greeting stream
+      greeting: 'greeting'
+    },
     streams: {
       main: 'getWeather',
       getWeather: [collectCity, provideWeather],
+          // Add a Stream for greetings and assign it a Step
+    greeting: handleGreeting,
+    main: 'onboarding',
+    onboarding: [sayHello],
+    end: [untrained]
     }
   })
 }
